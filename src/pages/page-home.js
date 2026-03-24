97a640190c825fd592bb85c5d9791af3225468dd
Keedjud <inoewenger@gmail.com>
2026-03-24 16:36:00 +0100

    Remove "Playlists" heading from page-home component

--- src/pages/page-home.js
+++ src/pages/page-home.js
@@ -2,7 +2,6 @@ customElements.define("page-home", class extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <h1 class="hero">Bienvenue</h1>
-      <h4>Playlists</h4>
     `
   }
 })
